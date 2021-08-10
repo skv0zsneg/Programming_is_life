68708bba334d8ec18df5e88238c8650f67a39527
klimentlamonov <1_0_91@bk.ru>
2021-08-10 13:03:36 +0300

    [add] Добавил записик по Fluent Python.

--- fluent_python/collections_namedtuple.py
+++ fluent_python/collections_namedtuple.py
@@ -1,17 +1,10 @@
 from collections import namedtuple
+"""
+    namedtuple - это создание класса с полями, но без методов.
+"""
 
 
-def test_create_namedtuple(*fields):
-    """
-    namedtuple - это создание класса без методов, но с полями.
+Computer = namedtuple('Computer', ['processor', 'ram'])  # Создание класса Computer с полями processor и ram.
+my_comp = Computer('intel core i9', '8 gb')
 
-    >>> test_create_namedtuple('Intel Core i9', '8 Gb')
-    Computer(processor='Intel Core i9', ram='8 Gb')
-    """
-    Computer = namedtuple('Computer', ['processor', 'ram'])  # Создание класса Computer с полями processor и ram.
-    return Computer(*fields)
-
-
-if __name__ == "__main__":  # launch: python <file_name> -v
-    import doctest
-    doctest.testmod(name='test_create_namedtuple', verbose=True)
+print(my_comp)  # Computer(processor='intel core i9', ram='8 gb')
--- fluent_python/random_choice.py
+++ fluent_python/random_choice.py
@@ -1,17 +1,8 @@
 from random import choice
-
-
-def test_choice(subsequence_object):
-    """
+"""
     choice -- это выбор случайного элемента из последовательности.
-    
-    >>> res = test_choice([1, 2, 3, 4, 5])
-    >>> res in [1, 2, 3, 4, 5]
-    True
-    """
-    return choice(subsequence_object)
+"""
 
+res = choice([1, {'oh': 'my'}, 'GoD!'])
 
-if __name__ == "__main__":  # launch: python <file_name> -v
-    import doctest
-    doctest.testmod(name='test_choice', verbose=True)
+print(res)  # 1 or {'oh': 'my'} or GoD!
