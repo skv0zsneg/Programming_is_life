238be98cc97166f6a6d55b73c267cec1083f7051
klimentlamonov <1_0_91@bk.ru>
2021-09-09 14:52:30 +0300

    add new notes

---
+++ fluent_python/dict_from_collections_tricks.py
@@ -0,0 +1,42 @@
+from collections import defaultdict, OrderedDict, ChainMap, Counter, UserDict
+
+
+"""
+    В defaultdict используется специальный метод __missing__.
+"""
+
+dd = defaultdict(list)
+KEY = 'mmm'
+print(dd[KEY])  # Если значения в словаре dd по ключу KEY нет, создается запись в словаре dd со значением [].
+print(dd.get(KEY))  # Поиск через get успешен, т.к. выше создалась такая запись.
+print(dd.get('?')) # Поиск провален, т.к. запись с ключом '?' не создана.
+
+
+"""
+    OrderedDict - отображение, в котором ключи хранятся так же как и задавались. Гарантируя фиксированную последовательность.
+"""
+od = OrderedDict(one=1, two=2, three=3)
+print(od)  # OrderedDict([('one', 1), ('two', 2), ('three', 3)])
+print(od['one'], od['three'])  # 1 3
+# .popitem(last=Union[True, False]) Удаляет и возвращает первый элемент из последовательности, если last=True. 
+# И последний, если last=False.
+print(od.popitem(last=True))  # ('three', 3)
+print(od.popitem(last=False))  # ('one', 1)
+
+
+"""
+    ChainMap - хранение нескольких словарей в одном инстансе для одновременного доступа ко всем из них, через объект, как к одному.
+"""
+cm = ChainMap({'one': 1}, {'a': 'A'}, {'42': 21})
+print(cm['one'], cm['42'], cm['a'])  # 1 21 A
+print([(k, v) for k, v in cm.items()])  # [('42', 21), ('a', 'A'), ('one', 1)]
+
+
+"""
+    Counter - отображение, в котором с каждым ключом ассоциирован счетчик. Принемает итерируемый объект.
+"""
+c = Counter('123123123abcabcABCABC')
+print(c)  # Counter({'1': 3, '2': 3, '3': 3, 'a': 2, 'b': 2, 'c': 2, 'A': 2, 'B': 2, 'C': 2})
+c.update((1, 2, 3, 4))
+print(c)  # Counter({'1': 3, '2': 3, '3': 3, 'a': 2, 'b': 2, 'c': 2, 'A': 2, 'B': 2, 'C': 2, 1: 1, 2: 1, 3: 1, 4: 1})
+
